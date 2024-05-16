c3b14b10dc483031cef38f8e06663abf395a70e6
AbishekJUNGthakuri19 <abishekjung00@gmail.com>
2024-05-16 22:19:34 +0545

    Category

--- src/Components/FoodDisplay/FoodDisplay.jsx
+++ src/Components/FoodDisplay/FoodDisplay.jsx
@@ -12,9 +12,12 @@ export const FoodDisplay = ({category}) => {
         <h2>Top Dishes Near You</h2>
         <div className="food-display-list">
             {
-            food_list.map((item,index)=>
-            <FoodItem key = {index} id={item._id} name={item.name} description={item.description} price={item.price} image = {item.image}  />
-            )
+            food_list.map((item,index)=>{
+              if (category === "All" || category === item.category){
+               return <FoodItem key = {index} id={item._id} name={item.name} description={item.description} price={item.price} image = {item.image}  />
+              }
+            })
+           
             }
         </div>
     </div>
