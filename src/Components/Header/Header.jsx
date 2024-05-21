2acd1c5aa199a76d22c7e0210aef611834af66ad
imxitiz <kshitizcoc123@gmail.com>
2024-05-21 23:54:50 +0545

    chore: Update header background image URL in Header component

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import './Header.css'
+import headerImg from '../../assets/header_img.png';
 
 export const Header = () => {
   return (
-    <div className='header'>
+    <div className='header' style={{ '--header-img': `url(${headerImg})` }}>
         <div className="header-contents">
             <h2>Order your favourite food here</h2>
             <p>Choose from a diverse menu featuring a delectable array of dishes crafted
